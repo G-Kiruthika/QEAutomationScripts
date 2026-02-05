7cb697566a075cb3ba097a99f8e59d2ec572e9de
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2026-02-06 00:01:20 +0530

    Deploying Page Objects

--- Scripts/TestScripts.py
+++ Scripts/TestScripts.py
@@ -22,3 +22,13 @@ class TestLoginFunctionality:
         expected_error = 'Invalid username or password. Please try again.'
         result = self.login_page.login_with_invalid_credentials_and_verify_error(username, password, expected_error)
         assert result, f"Expected error message '{expected_error}', but got something else."
+
+    def test_TC_LOGIN_002(self):
+        """
+        Test Case TC_LOGIN_002: Navigate to login screen and verify 'Remember Me' checkbox is absent.
+        Steps:
+        1. Navigate to the login screen.
+        2. Assert that 'Remember Me' checkbox is not present.
+        """
+        self.login_page.go_to_login_page()
+        self.login_page.assert_remember_me_checkbox_absent()
