758ef38c56981f3b1ba6308f607263e3293602b1
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2026-02-13 19:53:26 +0530

    Architect: Synchronizing Page Objects with UI Metadata.

--- auto_scripts/Pages/login_page.py
+++ auto_scripts/Pages/login_page.py
@@ -2,32 +2,46 @@ from selenium.webdriver.common.by import By
 from auto_scripts.Pages.base_page import BasePage
 
 class LoginPage(BasePage):
-    USERNAME_INPUT = (By.ID, "username")
-    PASSWORD_INPUT = (By.ID, "password")
-    LOGIN_BUTTON = (By.ID, "loginBtn")
-    # Added from metadata
-    ADMIN_LOGIN_LOCATOR = (By.ID, "placeholder_admin_login_locator")
-    ADMIN_LOGIN_SUCCESS_LOCATOR = (By.ID, "placeholder_admin_login_success_locator")
+    # Locators
+    LOGIN_URL = "https://example.com/login"  # Example URL, replace with actual if available
+    EMAIL_INPUT = (By.ID, "email_input")
+    PASSWORD_INPUT = (By.ID, "password_input")
+    LOGIN_BUTTON = (By.ID, "login_button")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[@class='success']")
+    USER_SESSION_INDICATOR = (By.ID, "user_session")
+    INVALID_USERNAME_INPUT = (By.ID, "username_input_invalid")
+    VALID_PASSWORD_INPUT = (By.ID, "password_input_valid")
+    FAILURE_MESSAGE = (By.XPATH, "//div[@class='error']")
+    LOGIN_PAGE_INDICATOR = (By.ID, "login_page_indicator")
 
-    def enter_username(self, username):
-        self.send_keys(self.USERNAME_INPUT, username)
+    # Actions
+    def navigate_to_login(self):
+        self.driver.get(self.LOGIN_URL)
+
+    def enter_email(self, email):
+        self.enter_text(self.EMAIL_INPUT, email)
 
     def enter_password(self, password):
-        self.send_keys(self.PASSWORD_INPUT, password)
+        self.enter_text(self.PASSWORD_INPUT, password)
 
     def click_login(self):
         self.click(self.LOGIN_BUTTON)
 
-    def is_login_button_visible(self):
-        return self.is_visible(self.LOGIN_BUTTON)
+    def enter_invalid_username(self, username):
+        self.enter_text(self.INVALID_USERNAME_INPUT, username)
 
-    # Added from metadata
-    def login_as_admin(self, username, password):
-        """Login as admin user using valid credentials."""
-        self.send_keys(self.ADMIN_LOGIN_LOCATOR, username)
-        self.send_keys(self.ADMIN_LOGIN_LOCATOR, password)
-        self.click(self.LOGIN_BUTTON)
+    def enter_valid_password(self, password):
+        self.enter_text(self.VALID_PASSWORD_INPUT, password)
 
-    def assert_admin_login_successful(self):
-        """Verify admin login was successful."""
-        return self.is_visible(self.ADMIN_LOGIN_SUCCESS_LOCATOR)
+    # Validations
+    def verify_login_success(self):
+        return self.is_element_visible(self.SUCCESS_MESSAGE)
+
+    def verify_user_session(self):
+        return self.is_element_visible(self.USER_SESSION_INDICATOR)
+
+    def verify_login_failure(self):
+        return self.is_element_visible(self.FAILURE_MESSAGE)
+
+    def verify_remain_on_login(self):
+        return self.is_element_visible(self.LOGIN_PAGE_INDICATOR)
