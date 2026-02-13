609288b0b60a5c7b9abe1c0ef9384139f1439071
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2026-02-13 11:00:28 +0530

    Architect: Synchronizing Page Objects with UI Metadata.

--- auto_scripts/Pages/registration_page.py
+++ auto_scripts/Pages/registration_page.py
@@ -2,49 +2,45 @@ from selenium.webdriver.common.by import By
 from auto_scripts.Pages.base_page import BasePage
 
 class RegistrationPage(BasePage):
-    EMAIL_INPUT = (By.ID, "email_input")
-    SUBMIT_BUTTON = (By.ID, "submit_button")
-    ERROR_MESSAGE = (By.ID, "error_message")
-    CONFIRMATION_MESSAGE = (By.ID, "confirmation_message")
+    EMAIL_INPUT = (By.XPATH, "//input[@id='email']")
+    SUBMIT_BUTTON = (By.XPATH, "//button[@id='register']")
+    ERROR_MESSAGE = (By.XPATH, "//div[@class='error']")
+    CONFIRMATION_MESSAGE = (By.XPATH, "//div[@class='success']")
 
-    # Newly appended locators
-    FIRST_NAME_INPUT = (By.ID, "first_name_input")
-    LAST_NAME_INPUT = (By.ID, "last_name_input")
-    PASSWORD_INPUT = (By.ID, "password_input")
-    REGISTER_BUTTON = (By.ID, "register_button")
-    SUCCESS_MESSAGE = (By.ID, "success_message")
+    # Missing locators appended below
+    FIRST_NAME_INPUT = (By.XPATH, "//input[@id='first_name']")
+    LAST_NAME_INPUT = (By.XPATH, "//input[@id='last_name']")
+    PASSWORD_INPUT = (By.XPATH, "//input[@id='password']")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[@class='success']")
     
+    # Existing methods
     def enter_email(self, email):
-        self.enter_text(self.EMAIL_INPUT, email)
+        self.driver.find_element(*self.EMAIL_INPUT).send_keys(email)
 
     def submit_registration(self):
-        self.click(self.SUBMIT_BUTTON)
+        self.driver.find_element(*self.SUBMIT_BUTTON).click()
 
     def is_confirmation_displayed(self):
-        return self.is_visible(self.CONFIRMATION_MESSAGE)
+        return self.is_element_visible(self.CONFIRMATION_MESSAGE)
 
     def is_error_displayed(self):
-        return self.is_visible(self.ERROR_MESSAGE)
+        return self.is_element_visible(self.ERROR_MESSAGE)
 
-    # Newly appended methods
+    # Missing methods appended below
     def enter_first_name(self, first_name):
-        self.enter_text(self.FIRST_NAME_INPUT, first_name)
+        self.driver.find_element(*self.FIRST_NAME_INPUT).send_keys(first_name)
 
     def enter_last_name(self, last_name):
-        self.enter_text(self.LAST_NAME_INPUT, last_name)
+        self.driver.find_element(*self.LAST_NAME_INPUT).send_keys(last_name)
 
     def enter_password(self, password):
-        self.enter_text(self.PASSWORD_INPUT, password)
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
 
     def click_register_button(self):
-        self.click(self.REGISTER_BUTTON)
+        self.driver.find_element(*self.SUBMIT_BUTTON).click()
 
     def get_success_message(self):
-        if self.is_visible(self.SUCCESS_MESSAGE):
-            return self.get_text(self.SUCCESS_MESSAGE)
-        return None
+        return self.driver.find_element(*self.SUCCESS_MESSAGE).text
 
     def get_error_message(self):
-        if self.is_visible(self.ERROR_MESSAGE):
-            return self.get_text(self.ERROR_MESSAGE)
-        return None
+        return self.driver.find_element(*self.ERROR_MESSAGE).text
