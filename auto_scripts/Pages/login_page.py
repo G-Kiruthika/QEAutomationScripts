c610b3030fe3a33d8425ac29812e698a673217a1
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2026-02-13 18:51:23 +0530

    Architect: Synchronizing Page Objects with UI Metadata.

--- auto_scripts/Pages/login_page.py
+++ auto_scripts/Pages/login_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
+from framework.base_page import BasePage  # Ensure this import matches your project structure
 
-class LoginPage:
+class LoginPage(BasePage):
     LOGIN_URL = "https://example.com/login"
     USERNAME_INPUT = (By.ID, "username")
     PASSWORD_INPUT = (By.ID, "password")
@@ -9,26 +10,26 @@ class LoginPage:
     ACCOUNT_LOCKOUT_MESSAGE = (By.CSS_SELECTOR, ".account-lockout")
     EMAIL_NOTIFICATION = (By.CSS_SELECTOR, ".email-notification")
 
-    def navigate_to_login_page(self, driver):
-        driver.get(self.LOGIN_URL)
+    def navigate_to_login_page(self):
+        self.driver.get(self.LOGIN_URL)
 
-    def enter_username(self, driver, username):
-        driver.find_element(*self.USERNAME_INPUT).send_keys(username)
+    def enter_username(self, username):
+        self.send_keys(self.USERNAME_INPUT, username)
 
-    def enter_password(self, driver, password):
-        driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
+    def enter_password(self, password):
+        self.send_keys(self.PASSWORD_INPUT, password)
 
-    def click_login_button(self, driver):
-        driver.find_element(*self.LOGIN_BUTTON).click()
+    def click_login_button(self):
+        self.click(self.LOGIN_BUTTON)
 
-    def verify_validation_error(self, driver):
-        return driver.find_element(*self.VALIDATION_ERROR).is_displayed()
+    def verify_validation_error(self):
+        return self.is_displayed(self.VALIDATION_ERROR)
 
-    def verify_login_prevented(self, driver):
-        return not driver.find_element(*self.LOGIN_BUTTON).is_enabled()
+    def verify_login_prevented(self):
+        return not self.is_enabled(self.LOGIN_BUTTON)
 
-    def verify_account_lockout_message(self, driver):
-        return driver.find_element(*self.ACCOUNT_LOCKOUT_MESSAGE).is_displayed()
+    def verify_account_lockout_message(self):
+        return self.is_displayed(self.ACCOUNT_LOCKOUT_MESSAGE)
 
-    def verify_email_notification_sent(self, driver):
-        return driver.find_element(*self.EMAIL_NOTIFICATION).is_displayed()
+    def verify_email_notification_sent(self):
+        return self.is_displayed(self.EMAIL_NOTIFICATION)
